bb73ca6255300362da39bc668d4113908789d763
Dat Le <dat.le@zalora.com>
2014-01-15 13:01:40 +0800

    Query which contains more than 1 statements will be ignored.

--- Redsift/DB.hs
+++ Redsift/DB.hs
@@ -9,15 +9,16 @@ import qualified Database.PostgreSQL.LibPQ as PQ
 import Database.PostgreSQL.Simple hiding (query)
 import qualified Data.Map as Map
 import qualified Data.Text as Text
-import Data.String.Utils (strip, join)
 import Data.Char (toLower)
-import Data.List.Split (splitOn)
 
 host = "redcat.zalora.com"
 port = 5439
 user = "zlall"
 db = "redcat"
 
+-- for exporting feature
+s3bucket = "s3://zalora-redsift-oregon"
+maxlimit = 2147483647
 connect' = connect defaultConnectInfo {connectHost = host, connectPort = port, connectUser = user, connectDatabase = db}
 
 allTables :: IO (Map.Map String [(String, Bool)])
@@ -34,38 +35,37 @@ allTables = (foldl' f Map.empty) `fmap` (flip query_ "SELECT table_schema, table
 -- representing Null (especially in homogenous lists) without them.
 query :: String -> Integer -> IO Aeson.Value
 query q limit = do
-  c <- PQ.connectdb $ BU.fromString $ "host=" ++ host ++ " port=" ++ show port ++ " dbname=" ++ db ++ " user=" ++ user
-  r' <- PQ.exec c $ BU.fromString $ limitQuery limit q
-  case r' of
-    Nothing -> (error . BU.toString . fromJust) `fmap` PQ.errorMessage c
-    Just r -> do
-      status <- PQ.resultStatus r
-      case status of
-        PQ.CommandOk -> return Aeson.Null -- shouldn't happen because web users shouldn't have write access
-        PQ.TuplesOk -> tuples r
-        _ -> (error . BU.toString . fromJust) `fmap` PQ.resultErrorMessage r
- where tuples r = do
-         nTuples <- PQ.ntuples r
-         nFields <- PQ.nfields r
-         names <- mapM (\x -> fromJust `fmap` PQ.fname r x) [0 .. nFields - 1]
-         rows <- mapM (getRow r nFields) [0 .. nTuples - 1]
-         return $ Aeson.toJSON $ [map (Aeson.String . Text.pack . BU.toString) names] ++ rows
-       getRow r nFields rowNum = mapM (\x -> toValue `fmap` PQ.getvalue r rowNum x) [0 .. nFields - 1]
-       toValue v = case v of
-                     Nothing -> Aeson.Null
-                     Just s -> Aeson.String $ Text.pack $ BU.toString s
+  if (isSingleQuery q) then do
+      c <- PQ.connectdb $ BU.fromString $ "host=" ++ host ++ " port=" ++ show port ++ " dbname=" ++ db ++ " user=" ++ user
+      r' <- PQ.exec c $ BU.fromString $ limitQuery limit q
+      case r' of
+        Nothing -> (error . BU.toString . fromJust) `fmap` PQ.errorMessage c
+        Just r -> do
+          status <- PQ.resultStatus r
+          case status of
+            PQ.CommandOk -> return Aeson.Null -- shouldn't happen because web users shouldn't have write access
+            PQ.TuplesOk -> tuples r
+            _ -> (error . BU.toString . fromJust) `fmap` PQ.resultErrorMessage r
+  else error "Multiple queries is not allowed."
+  where isSingleQuery q0 = length (filter (== ';') q0) < 2
+        tuples r = do
+          nTuples <- PQ.ntuples r
+          nFields <- PQ.nfields r
+          names <- mapM (\x -> fromJust `fmap` PQ.fname r x) [0 .. nFields - 1]
+          rows <- mapM (getRow r nFields) [0 .. nTuples - 1]
+          return $ Aeson.toJSON $ [map (Aeson.String . Text.pack . BU.toString) names] ++ rows
+        getRow r nFields rowNum = mapM (\x -> toValue `fmap` PQ.getvalue r rowNum x) [0 .. nFields - 1]
+        toValue v = case v of
+                      Nothing -> Aeson.Null
+                      Just s -> Aeson.String $ Text.pack $ BU.toString s
 
 -- in case User's Defined Query doesn't limit number of rows, or return too many rows than allowed
+-- For this method, query q is assumed to have AT MOST one semicolon,
+-- otherwise it would result in error in the query function
 limitQuery :: Integer -> String -> String
-limitQuery limit q =  join ";" $ map (applyLimit limit) $ splitQuery q
-
-applyLimit :: Integer -> String -> String
-applyLimit limit q = let qAsList = words q in
+limitQuery limit q = let qAsList = words $ filter (/=';') q in
                      case (map toLower (qAsList !! (length qAsList - 2))) of
                        "limit" -> case (read (qAsList !! (length qAsList - 1))) > limit of
                                     True -> unwords $ (init qAsList) ++ [show limit]
                                     False -> unwords $ qAsList
                        _ -> unwords $ qAsList ++ ["limit", show limit]
-
-splitQuery :: String -> [String]
-splitQuery q = filter (not . null) $ map strip $ splitOn ";" q
