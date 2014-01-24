627aefd1865019229066e6bc45e1ae1544ff5b70
Dat Le <dat.le@zalora.com>
2014-01-24 13:13:53 +0800

    Properly handle *unlimit-able query* / empty query.

--- Redsift/DB.hs
+++ Redsift/DB.hs
@@ -18,6 +18,7 @@ import Network.AWS.AWSConnection
 import System.Locale
 import Control.Applicative
 import Control.Exception (bracket)
+import Safe
 
 import Redsift.Exception
 import Redsift.SignUrl
@@ -42,19 +43,22 @@ allTables connectInfo = withDB connectInfo $ \db -> foldl' f Map.empty `fmap` qu
 query :: ConnectInfo -> String -> AppConfig -> IO Aeson.Value
 query connectInfo q (AppConfig _ rowLimit) = withDB connectInfo $ \db -> withConnection db $ \db' ->
   if isSingleQuery q then do
-      r' <- PQ.exec db' $ BU.fromString $ limitQuery rowLimit q
-      case r' of
-        Nothing -> do
-          err <- (BU.toString . fromJust) `fmap` PQ.errorMessage db'
-          throwUserException err
-        Just r -> do
-          status <- PQ.resultStatus r
-          case status of
-            PQ.CommandOk -> return Aeson.Null -- shouldn't happen because web users shouldn't have write access
-            PQ.TuplesOk -> tuples r
-            _ -> do
-              err <- (BU.toString . fromJust) `fmap` PQ.resultErrorMessage r
-              throwUserException err
+    case (limitQuery rowLimit q) of
+      Just query -> do
+        r' <- PQ.exec db' $ BU.fromString query
+        case r' of
+          Nothing -> do
+            err <- (BU.toString . fromJust) `fmap` PQ.errorMessage db'
+            throwUserException err
+          Just r -> do
+            status <- PQ.resultStatus r
+            case status of
+              PQ.CommandOk -> return Aeson.Null -- shouldn't happen because web users shouldn't have write access
+              PQ.TuplesOk -> tuples r
+              _ -> do
+                err <- (BU.toString . fromJust) `fmap` PQ.resultErrorMessage r
+                throwUserException err
+      Nothing -> throwUserException "The given query is not allowed."
   else throwUserException "Multiple queries is not allowed."
   where isSingleQuery q
           |';' `notElem` q = True -- No semicolons
@@ -75,13 +79,16 @@ query connectInfo q (AppConfig _ rowLimit) = withDB connectInfo $ \db -> withCon
 
 export :: ConnectInfo -> String -> String -> String -> S3Config -> GmailConfig -> IO Aeson.Value
 export connectInfo recipient reportName q s3Config gmailConfig = do
-  forkIO $ withDB connectInfo $ \db -> withConnection db $ \db' -> do
-    s3Prefix <- createS3Prefix recipient reportName
-    r' <- PQ.exec db' $ BU.fromString $ unloadQuery q s3Prefix s3Config
-    case r' of
-      Nothing -> throwUserException =<< ((BU.toString . fromJust) `fmap` PQ.errorMessage db')
-      Just _ -> processSuccessExport s3Prefix recipient s3Config gmailConfig
-  return $ Aeson.toJSON $ Aeson.String "Your export request has been sent. The export URL will be sent to your email shortly."
+  s3Prefix <- createS3Prefix recipient reportName
+  case (unloadQuery q s3Prefix s3Config) of
+    Just unload -> do
+      forkIO $ withDB connectInfo $ \db -> withConnection db $ \db' -> do
+        r' <- PQ.exec db' $ BU.fromString unload
+        case r' of
+          Nothing -> throwUserException =<< ((BU.toString . fromJust) `fmap` PQ.errorMessage db')
+          Just _ -> processSuccessExport s3Prefix recipient s3Config gmailConfig
+      return $ Aeson.toJSON $ Aeson.String "Your export request has been sent. The export URL will be sent to your email shortly."
+    Nothing -> throwUserException "The given query is not allowed."
 
 -- Generate S3 Location, based on current Date, current Time, export Name, and recipient Email
 createS3Prefix :: String -> String -> IO String
@@ -92,17 +99,19 @@ createS3Prefix recipient reportName = do
 
 -- Wrap normal query with UNLOAD statement and 2147483647 as MaxLimit so that the result will be just one file on S3
 -- refer to: https://bitbucket.org/zalorasea/redsift/issue/3/export-to-csv
-unloadQuery :: String -> String -> S3Config -> String
-unloadQuery q s3Prefix (S3Config bucket access secret _) = 
-  "UNLOAD ('SELECT * FROM ("
-  ++ limitQuery 2147483647 q
-  ++ " )') to '"
-  ++ "s3://" ++ bucket ++ "/" ++ s3Prefix
-  ++ "' credentials 'aws_access_key_id="
-  ++ access
-  ++ ";aws_secret_access_key="
-  ++ secret
-  ++ "'ALLOWOVERWRITE GZIP;"
+unloadQuery :: String -> String -> S3Config -> Maybe String
+unloadQuery q s3Prefix (S3Config bucket access secret _) = do
+  case (limitQuery 2147483647 q) of
+    Just query -> Just $ "UNLOAD ('SELECT * FROM ("
+                    ++ query
+                    ++ " )') to '"
+                    ++ "s3://" ++ bucket ++ "/" ++ s3Prefix
+                    ++ "' credentials 'aws_access_key_id="
+                    ++ access
+                    ++ ";aws_secret_access_key="
+                    ++ secret
+                    ++ "'ALLOWOVERWRITE GZIP;"
+    Nothing -> Nothing
 
 -- Once Data is exported to S3, find the gz export, send email accordingly
 processSuccessExport :: String -> String -> S3Config -> GmailConfig -> IO ()
@@ -120,14 +129,17 @@ processSuccessExport s3Prefix recipient (S3Config bucket access secret expiry) (
 -- In case User's Defined Query doesn't limit number of rows, or return too many rows than allowed
 -- For this method, query q is assumed to have AT MOST one semicolon,
 -- otherwise it would result in error in the query function
-limitQuery :: Int -> String -> String
+limitQuery :: Int -> String -> Maybe String
 limitQuery limit q = let qAsList = words $ filter (/=';') q in
-                     case map toLower (qAsList !! (length qAsList - 2)) of
-                       "limit" -> unwords $
-                          if read (qAsList !! (length qAsList - 1)) > limit
-                                  then init qAsList ++ [show limit]
-                                  else qAsList
-                       _ -> unwords $ qAsList ++ ["limit", show limit]
+                     case (fmap (map toLower) (qAsList `atMay` (length qAsList - 2))) of
+                        Just "limit" -> case (qAsList `atMay` (length qAsList - 1)) of
+                            Just m -> case readMay m of
+                              Just n -> if n > limit then Just $ unwords $ init qAsList ++ [show limit]
+                                        else Just q
+                              Nothing -> Nothing
+                            Nothing -> Nothing
+                        Just _ -> Just $ unwords $ qAsList ++ ["limit", show limit]
+                        Nothing -> Nothing
 
 -- Make sure a connection is closed after we finished with the query.
 withDB :: ConnectInfo -> (Connection -> IO a) -> IO a
