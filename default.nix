6a59dd0ade469531fdf348b44553a37f422ffeab
Shea Levy <shea.levy@zalora.com>
2014-02-19 07:38:06 -0500

    zalora-nix-lib is the source, nix-lib is the set
    Fix to work with hydra

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {}
 , src ? ./. # Eventually want to filter out ignores
-, zalora-nix-lib ? import <zalora-nix-lib> { inherit pkgs; }
+, zalora-lib ? import <zalora-nix-lib> { inherit pkgs; }
 }:
 
 {
-  build = zalora-nix-lib.buildLocalCabal src "redsift";
+  build = zalora-lib.buildLocalCabal src "redsift";
 }
