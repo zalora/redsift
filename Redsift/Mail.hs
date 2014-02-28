765e7e5ac90cc3d33e252e620a1da104ca7e8492
Sönke Hahn <soenkehahn@gmail.com>
2014-02-28 16:05:17 +0800

    switching output for sending emails (back?) to stderr

--- Redsift/Mail.hs
+++ Redsift/Mail.hs
@@ -11,7 +11,7 @@ sendCSVExportMail config recipient url =
     let subj = "Your Redsift Export"
         body = "Hi Redsift User, \n\n Here's your exported data URL:\n" ++ url ++ "\n\nThanks,\n\n This email was sent on behalf of Zalora DataScience team"
     in do
-        putStrLn $ "Sending to " ++ recipient ++ ":\n" ++ url
+        hPutStrLn stderr $ "Sending to " ++ recipient ++ ":\n" ++ url
         sendMail config recipient subj body
 
 sendMail :: GmailConfig -> String -> String -> String -> IO ()
@@ -19,4 +19,4 @@ sendMail (GmailConfig from password) recipient subj body =
     let to   = [recipient]
         cc   = []
         bcc  = []
-    in sendGmail stdout from password to cc bcc subj body
+    in sendGmail stderr from password to cc bcc subj body
