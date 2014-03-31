2599333036b5f0c805a8b6a8d6f55e91d4e3353b
Simon Hengel <sol@typeful.net>
2014-03-31 16:23:35 +0800

    Depend on mime-mail >= 0.4.4.2

--- Redsift/Mail.hs
+++ Redsift/Mail.hs
@@ -1,6 +1,6 @@
 module Redsift.Mail where
 
-import Network.Mail.Mime (Mail(..), renderSendMailCustom, plainPart)
+import Network.Mail.Mime (Mail(..), renderSendMail, plainPart)
 import System.IO
 import Data.String.Conversions
 
@@ -17,7 +17,7 @@ sendCSVExportMail config recipient url =
 
 sendMail :: EmailConfig -> Address -> String -> String -> IO ()
 sendMail (EmailConfig from) recipient subj body =
-    renderSendMailCustom "sendmail" ["-t"] $ Mail {
+    renderSendMail $ Mail {
             mailFrom = from,
             mailTo = [recipient],
             mailCc = [],
