zalora/redsift

commit aa654698a81adb77b71acb9323590e64a6b8b318

looking for www in the current working directory to serve static files (for development)

redsift.hs
@@ -1,6 +1,7 @@
 {-# language OverloadedStrings #-}
 module Main where
 
+import Control.Exception
 import Data.Aeson (ToJSON(..), encode)
 import Data.ByteString (ByteString)
 import Data.Maybe
@@ -13,7 +14,7 @@ import Network.Wai.Handler.Warp hiding (Connection)
 import Network.Wai.Application.Static
 import Network.Wai.UrlMap
 import Safe
-import System.FilePath
+import System.Directory
 import System.IO
 
 import Paths_redsift
@@ -46,6 +47,18 @@ optionsParser =
 defaultConfigFile :: FilePath
 defaultConfigFile = "./Config/redsift.config"
 
+-- | Chooses './www' if that exists, uses cabal's data-files mechanism
+-- otherwise.
+getDocumentRoot :: IO FilePath
+getDocumentRoot = do
+    wwwExists <- doesDirectoryExist "www"
+    if wwwExists then return "www"
+    else do
+        cabalDataDir <- getDataDir
+        cabalDataDirExists <- doesDirectoryExist cabalDataDir
+        if cabalDataDirExists then return cabalDataDir
+        else throwIO (ErrorCall "directory for static files not found.")
+
 
 -- * main entry function
 
@@ -54,7 +67,8 @@ main = do
     options <- execParser optionsParser
     redsiftConfig <- readRedsiftConfig (fromMaybe defaultConfigFile (config options))
     let port = appPort $ app redsiftConfig
-    documentRoot <- (</> "www") <$> getDataDir
+    documentRoot <- getDocumentRoot
+    hPutStrLn stderr ("serving static files from " ++ documentRoot)
     let settings =
             setPort port $
             setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $

redsift.cabal
@@ -26,6 +26,7 @@ executable redsift
   build-depends:       aeson,
                        MissingH,
                        base >=4.6 && <4.7,
+                       directory,
                        bytestring,
                        configurator,
                        containers,

commit b1c0f3c97953e5a6ee1affa84d07add826649604

fix lookup of document root when cabal install'ed (bug introduced in aa654698a81adb77b71acb9323590e64a6b8b318)

redsift.hs
@@ -15,6 +15,7 @@ import Network.Wai.Application.Static
 import Network.Wai.UrlMap
 import Safe
 import System.Directory
+import System.FilePath
 import System.IO
 
 import Paths_redsift
@@ -56,8 +57,9 @@ getDocumentRoot = do
     else do
         cabalDataDir <- getDataDir
         cabalDataDirExists <- doesDirectoryExist cabalDataDir
-        if cabalDataDirExists then return cabalDataDir
-        else throwIO (ErrorCall "directory for static files not found.")
+        if cabalDataDirExists
+            then return (cabalDataDir </> "www")
+            else throwIO (ErrorCall "directory for static files not found.")
 
 
 -- * main entry function